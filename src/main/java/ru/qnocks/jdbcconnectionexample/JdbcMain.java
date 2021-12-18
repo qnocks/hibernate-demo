eb8cb47469efda7254837293ebe014b8ae08ccc0
qnocks <roma.ost2012@mail.ru>
2021-12-18 18:06:34 +0300

    Finished

--- src/main/java/ru/qnocks/jdbcconnectionexample/JdbcMain.java
+++ src/main/java/ru/qnocks/jdbcconnectionexample/JdbcMain.java
@@ -1,5 +1,7 @@
 package ru.qnocks.jdbcconnectionexample;
 
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.sql.*;
@@ -19,28 +21,12 @@ public class JdbcMain {
         Statement stm = con.createStatement();
         ResultSet res = stm.executeQuery(query);
 
-        String columns[] = { "ID", "Number", "Area", "Cost" };
-        String data[][] = new String[10][4];
-
-        int i = 0;
-        while (res.next()) {
-            long id = res.getLong(1);
-            String number = res.getString(2);
-            int area = res.getInt(3);
-            long cost = res.getLong(4);
-            data[i][0] = id + "";
-            data[i][1] = number;
-            data[i][2] = area + "";
-            data[i][3] = cost + "";
-            i++;
-        }
-
-        DefaultTableModel model = new DefaultTableModel(data, columns);
-        JTable table = new JTable(model);
+        JTable table = new JTable();
+        table.setModel(DbUtils.resultSetToTableModel(res));
         table.setShowGrid(true);
         table.setShowVerticalLines(true);
         JScrollPane pane = new JScrollPane(table);
-        JFrame f = new JFrame("Populate JTable from Database");
+        JFrame f = new JFrame("JDBC");
         JPanel panel = new JPanel();
         panel.add(pane);
         f.add(panel);
